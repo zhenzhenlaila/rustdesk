fa0152d345950485d553428748f780cf1dbe07ee
zhenzhenlaila <xiangzhenlin@tenpao.com>
2025-01-14 08:44:25 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,12 +909,12 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://172.19.2.2:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
     let url = get_api_server(api, custom);
-    if url.is_empty() || url.contains("rustdesk.com") {
+    if url.is_empty() || url.contains("") {
         return "".to_owned();
     }
     format!("{}/api/audit/{}", url, typ)
